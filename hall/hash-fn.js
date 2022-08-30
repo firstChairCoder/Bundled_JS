b4a2f90fd5f98127e4e5956d5e8b37952fcc5de3
firstChairCoder <md1qq7@gmail.com>
2022-08-30 12:20:05 +0100

    chore: serve hashing fn.

---
+++ hall/hash-fn.js
@@ -0,0 +1,12 @@
+var vapulusHash = require('vapulus-hashing-pkg');
+
+var myRequestParams = {
+    cardNum : '5123456789012346',
+    cardExp : 2105,
+    cardCVC : 123,
+    holderName :'John Doe',
+    mobileNumber :'20100000000000'
+};
+var hashSecret='C0DF9A7B3819968807A9D4E48D0E65C6';
+
+var secureHash = vapulusHash.generateHash(hashSecret,myRequestParams);
